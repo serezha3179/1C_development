08b528eba0674428a89f95993652469e6d0653ad
serezha3179 <sergeiabramkin.79@gmail.com>
2023-05-27 22:05:36 +0300

    change header

---
+++ src/js/header.js
@@ -0,0 +1,16 @@
+if(document.querySelector('header')) {
+    const header = document.querySelector('.header');
+    const headerBurger = document.querySelector('.header__burger');
+    const navigation = document.querySelectorAll('.nav__link')
+
+    navigation.forEach(item=>item.addEventListener('click', (e)=> {
+        e.preventDefault()
+        navigation.forEach(item=>item.classList.remove('active'));
+        item.classList.add('active')
+    }));
+
+    headerBurger.addEventListener('click', ()=>{
+        headerBurger.classList.toggle('active');
+        header.classList.toggle('active')
+    })
+}
